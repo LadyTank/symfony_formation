a0175c246f69a5545189251dd7f3d6c12cd5121a
Katia Semedo <katia.semedo@10mentionweb.org>
2021-04-26 15:02:00 +0200

    twig 26/04

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -14,6 +14,37 @@ class ArticlesController extends AbstractController
  */
     public function index()
     {
-        return $this -> render('liste.html.twig');
+        $articles = []; //création du tableau
+
+        $articles [] = [ // ajout d'un tableau dans le tableau $articles
+            'id' => 1,
+            'auteur' => 'Katia',
+            'titre' => 'Un titre',
+            'description' => 'lorem ipsum',
+            'date_publication' => '2021-05-01'
+        ];
+
+        $articles [] = [
+            'id' => 2,
+            'auteur' => 'Justine',
+            'titre' => 'Un titre2',
+            'description' => 'lorem ipsum',
+            'date_publication' => '2021-05-02'
+        ];
+
+        $articles [] = [
+            'id' => 3,
+            'auteur' => 'Audrey',
+            'titre' => 'Un titre3',
+            'description' => 'lorem ipsum',
+            'date_publication' => '2021-05-03'
+        ];
+
+        // fonctionnalité de symfony équivalente au var_dump. Le resultat sera indiqué dans le profileur sur le navigateur
+        dump($articles);
+
+        return $this -> render('articles/index.html.twig', [
+            'posts' => $articles
+        ]);
     }
 }
